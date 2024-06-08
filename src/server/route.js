a04222c36152b09ad500cdc29203d275dd2adf8d
Synmary <ardyprasyah@gmail.com>
2024-06-08 13:35:07 +0000

    login dan reset password, tapi untuk reset apssword cumna berlaku ke user saja, blm dokter

--- src/server/route.js
+++ src/server/route.js
@@ -2,6 +2,7 @@ const express = require('express');
 const { userRegistration } = require('../user/pet owner/regisHandler');
 const { userLogin } = require('../user/pet owner/loginHandler');
 const emailLinkVerificator = require('../verification/verifiedFromEmailLink');
+const { userResetPasswordReq, userResetPasswordVerification, userResetPasswordInput } = require('../password reset/resetPasswordHandler');
 const router = express.Router();
 
 // Define the routes
@@ -14,5 +15,10 @@ router.get('/', (req, res) => {
 router.post('/loginUser', userLogin)
 router.post('/regisUser', userRegistration);
 router.get('/:id/verify/:token', emailLinkVerificator);
+
+//Forgot password Route
+router.post('/forgotPassword', userResetPasswordReq);
+router.post('/forgotPassword/verification', userResetPasswordVerification);
+router.post('/forgotPassword/verification/input', userResetPasswordInput);
 
 module.exports = router;
